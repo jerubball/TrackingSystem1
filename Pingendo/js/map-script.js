80866deb33c54582817b51de16a43447325fadb3
Hasol Im <jerubball@gmail.com>
2018-04-15 23:17:32 -0400

    map.html update

---
+++ Pingendo/js/map-script.js
@@ -0,0 +1,14 @@
+
+function map1() {
+  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+  var mapCanvas = document.getElementById("map1");
+  var mapOptions = {center: myCenter, zoom: 5};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "Hello World!"
+  });
+  infowindow.open(map,marker);
+}
