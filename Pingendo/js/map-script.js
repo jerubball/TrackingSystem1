db74c2b8d8abd2cdcb5ec0c62396bc0f0185dd7c
Hasol Im <jerubball@gmail.com>
2018-04-21 22:33:12 -0400

    map update

--- Pingendo/js/map-script.js
+++ Pingendo/js/map-script.js
@@ -1,14 +1,25 @@
 
 function map1() {
-  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showMap1);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function showMap1 (position) {
+  var myCenter = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   var mapCanvas = document.getElementById("map1");
   var mapOptions = {center: myCenter, zoom: 5};
   var map = new google.maps.Map(mapCanvas, mapOptions);
   var marker = new google.maps.Marker({position:myCenter});
   marker.setMap(map);
-
   var infowindow = new google.maps.InfoWindow({
-    content: "Hello World!"
+    content: "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude
   });
   infowindow.open(map,marker);
+}
+
+function alertLocation (position) {
+  alert ("Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude);
 }
