fab41d35bfdbeabdd7d278ac9c15378061707cdf
alin4627 <alin4627@gmail.com>
2018-04-28 17:30:22 -0400

    fixed foreign keys

--- sql/create_table_sw_eng.sql
+++ sql/create_table_sw_eng.sql
@@ -1,54 +1,75 @@
--- DROP DATABASE IF EXISTS `Sft_Eng` ;
-CREATE SCHEMA IF NOT EXISTS `Sft_Eng` ;
-USE `Sft_Eng` ;
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Table `Sft_Eng`.`Family_Group`
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Sft_Eng`.`Family_Group` (
-  `Group_ID` INT NOT NULL,
-  `Street_Address` VARCHAR(50) NOT NULL,
-  `City_Address` VARCHAR(50) NOT NULL,
-  `State_Address` VARCHAR(50) NOT NULL,
-  `ZIP_Address` VARCHAR(10) NOT NULL,
+-- -----------------------------------------------------
+-- Schema Child_Tracker
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema Child_Tracker
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Child_Tracker` ;
+USE `Child_Tracker` ;
+
+-- -----------------------------------------------------
+-- Table `Child_Tracker`.`family_group`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Child_Tracker`.`family_group` (
+  `Group_ID` INT NOT NULL AUTO_INCREMENT,
+  `Street_Address` VARCHAR(45) NOT NULL,
+  `City_Address` VARCHAR(45) NOT NULL,
+  `State_Address` VARCHAR(45) NOT NULL,
+  `Zip_Address` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`Group_ID`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Sft_Eng`.`Account_Info`
+-- Table `Child_Tracker`.`account_info`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Sft_Eng`.`Account_Info` (
-  `Account_ID` INT NOT NULL,
-  `First_Name` VARCHAR(50) NOT NULL,
-  `Last_Name` VARCHAR(50) NOT NULL,
-  `Email` VARCHAR(50) NOT NULL,
-  `Phone_Number` CHAR(10) NOT NULL,
-  `Account_Type` ENUM('Parent', 'Child', 'Guardian', 'Teacher') NOT NULL,
-  `Gender` ENUM('Male', 'Female') NULL,
+CREATE TABLE IF NOT EXISTS `Child_Tracker`.`account_info` (
+  `Account_ID` INT NOT NULL AUTO_INCREMENT,
+  `First_Name` VARCHAR(45) NULL,
+  `Last_Name` VARCHAR(45) NULL,
+  `Email` VARCHAR(45) NOT NULL,
   `Group_ID` INT NOT NULL,
+  `Gender` ENUM('Male', 'Female') NULL,
+  `Account_Type` ENUM('Parent', 'Child') NOT NULL,
   PRIMARY KEY (`Account_ID`),
-  CONSTRAINT `Account_Group`
+  INDEX `Group_ID_idx` (`Group_ID` ASC),
+  CONSTRAINT `Group_ID`
     FOREIGN KEY (`Group_ID`)
-    REFERENCES `Sft_Eng`.`Family_Group` (`Group_ID`)
+    REFERENCES `Child_Tracker`.`family_group` (`Group_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Sft_Eng`.`Location`
+-- Table `Child_Tracker`.`location`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Sft_Eng`.`Location` (
-  `Account_ID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `Child_Tracker`.`location` (
   `Time` DATETIME NOT NULL,
   `Longitude` FLOAT NOT NULL,
   `Latitude` FLOAT NOT NULL,
-  `Status` ENUM('Urgent', 'Normal') NOT NULL,
-  PRIMARY KEY (`Account_ID`, `Time`),
-  CONSTRAINT `Location_Account`
+  `Status` ENUM('Normal', 'Emergency') NOT NULL,
+  `Account_ID` INT NOT NULL,
+  PRIMARY KEY (`Time`),
+  INDEX `Account_ID_idx` (`Account_ID` ASC),
+  CONSTRAINT `Account_ID`
     FOREIGN KEY (`Account_ID`)
-    REFERENCES `Sft_Eng`.`Account_Info` (`Account_ID`)
+    REFERENCES `Child_Tracker`.`account_info` (`Account_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
