6b9445b7af6a731113cc9fb81667a119c45fc789
Hasol Im <jerubball@gmail.com>
2018-04-30 01:18:15 -0400

    update 1

--- Pingendo/php/login-new.php
+++ Pingendo/php/login-new.php
@@ -20,7 +20,7 @@ if (isset($_SESSION['id'])) {
     
     $sql = "INSERT INTO Child_Tracker.account_info VALUES (DEFAULT, $id, $first, $last, $email, NULL, NULL, NULL)";
     $ans = $conn -> query($sql);
-    echo ;
+    echo $ans;
     mysqli_close($conn);
 }
 
