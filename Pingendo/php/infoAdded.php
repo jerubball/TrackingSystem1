b791ee910622a3adb98d54d63518c4c49a74136a
IvanTheHorrible <38411353+IvanTheHorrible@users.noreply.github.com>
2018-04-29 21:08:31 -0400

    test

--- Pingendo/php/infoAdded.php
+++ Pingendo/php/infoAdded.php
@@ -4,24 +4,13 @@
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
 <?php
-echo "hi";
-        $f_name = trim($_POST['first_name']);
-        $l_name = trim($_POST['last_name']);
-        $gender = trim($_POST['gender']);
-        $email = trim($_POST['email']);
+
+        echo $_POST["first_name"]);
+        
     
 
-require_once('../mysqli_connect.php');
-$query = "INSERT INTO account_Info('First_Name','Last_Name','Gender','Email',)
-	      VALUES:(?,?,?,?)"
-$stmt = mysqli_prepare($dbc, $query);
-	b Blobs 
-
-mysqli_stmt_bind_param($stmt,'ssss','f_name','l_name','gender','email')	
-mysqli_stmt_execute($stmt);
-mysqli_stmt_close($stmt);
-mysqli_close($dbc);
-
+// leave for now require_once('../mysqli_connect.php');
+//$query = "UPDATE account_Info SET first_name = $f_name, last_name = $l_name, gender = $gender, email = $email";
 
 ?>
 </body>
