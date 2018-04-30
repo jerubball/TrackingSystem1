ee61c6a84d0f8e9f43fc8fe1e25c98b7d5247a52
Hasol Im <jerubball@gmail.com>
2018-04-29 22:32:54 -0400

    script update

--- Pingendo/js/login-script.js
+++ Pingendo/js/login-script.js
@@ -23,6 +23,7 @@ function onSignIn(googleUser) {
     */
     
     updateSession (profile.getId());
+    redirectURL ("/main.html");
 }
 
 function signOut() {
@@ -42,4 +43,11 @@ function updateSession (id) {
     };
     xmlhttp.open ("GET", "/php/login-cache.php?id=" + id, true);
     xmlhttp.send ();
+}
+
+function redirectURL (url) {
+    // similar behavior as an HTTP redirect
+    window.location.replace(url);
+    // similar behavior as clicking on a link
+    //window.location.href = url;
 }
