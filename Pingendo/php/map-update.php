cb13f27089cdd7a9cff7e5a7296795c563338cc2
Hasol Im <jerubball@gmail.com>
2018-04-29 20:11:00 -0400

    typo fix.

--- Pingendo/php/map-update.php
+++ Pingendo/php/map-update.php
@@ -6,7 +6,7 @@ $lon = floatval ($_GET['lon']);
 $conn = new mysqli ("localhost:3306", "admin", "13579");
 
 if ($conn -> connect_error) {
-  die ("Connection failed: " . conn -> connect_error);
+  die ("Connection failed: " . $conn -> connect_error);
 }
 
 $now = time();
