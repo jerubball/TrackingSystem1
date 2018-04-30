dd5869487ae702f438ebde45cc97908d465eb20e
Hasol Im <jerubball@gmail.com>
2018-04-30 02:45:55 -0400

    Comments

--- Pingendo/php/infoAdded.php
+++ Pingendo/php/infoAdded.php
@@ -8,6 +8,12 @@ $servername = "him-nyit.ddns.net";
 $username = "admin";
 $password = "13579";
 $dbname = "Child_Tracker";
+/* my db arguments.
+// localhost seems to work. port number is important.
+$db_server = "localhost:3306";
+$db_user = "admin";
+$db_pass = "13579";
+*/
 
 if(isset($_POST["send"]))// User came to this page by using Create Group 
 {
@@ -31,6 +37,31 @@ else // If the user came to this page by using edit account info
 	  mysqli_query($conn, "UPDATE INTO account_info( First_Name, Last_Name, Email, Gender)
 						  VALUES(" . $fName . ", " . $lName . ", " . $email . ", " . $gender . ",)");
 }
+
+/**
+
+// start new connection
+    $conn = new mysqli ($db_server, $db_user, $db_pass);
+
+// this check for bad connection.
+    if ($conn -> connect_error) {
+      die ("Connection failed: " . $conn -> connect_error);
+    }
+
+// ignore this. this is variable for location DB.
+    $now = time();
+
+// this part just sets sql statement as string variable.
+    $sql = "INSERT INTO Child_Tracker.location VALUES ($usr, FROM_UNIXTIME($now), $lat, $lon, 'Normal')";
+
+// ths part actually executes sql statement.
+    $ans = $conn -> query($sql);
+    //echo $id;
+
+// it is recommended to close connection after connection.
+    mysqli_close($conn);
+
+*/
 
 ?>
 </body>
