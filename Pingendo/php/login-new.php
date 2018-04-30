787a1fcb7958cd2d054285750d37c05df5cc3ee0
Hasol Im <jerubball@gmail.com>
2018-04-30 01:35:53 -0400

    test 2

--- Pingendo/php/login-new.php
+++ Pingendo/php/login-new.php
@@ -12,18 +12,43 @@ if (isset($_SESSION['id'])) {
     $last = $_SESSION['last'];
     $email = $_SESSION['email'];
     
+    if (is_string($id)) {
+        echo "1";
+    }
+    else {
+        echo "2";
+    }
+    if (is_string($first)) {
+        echo "1";
+    }
+    else {
+        echo "2";
+    }
+    if (is_string($last)) {
+        echo "1";
+    }
+    else {
+        echo "2";
+    }
+    if (is_string($email)) {
+        echo "1";
+    }
+    else {
+        echo "2";
+    }
+    
     $conn = new mysqli ($db_server, $db_user, $db_pass);
     
     if ($conn -> connect_error) {
       die ("Connection failed: " . $conn -> connect_error);
     }
     
-    //$sql = "INSERT INTO Child_Tracker.account_info VALUES (DEFAULT, $id, $first, $last, $email, NULL, NULL, NULL)";
-    $sql = "SELECT * FROM Child_Tracker.account_info LIMIT 1";
+    $sql = "INSERT INTO Child_Tracker.account_info VALUES (DEFAULT, $id, $first, $last, $email, NULL, NULL, NULL)";
+    //$sql = "SELECT * FROM Child_Tracker.account_info LIMIT 1";
     $ans = $conn -> query($sql);
-    //echo $id." ".$first." ".$last." ".$email;
-    $row = mysqli_fetch_assoc($ans);
-    echo $row['Account_ID'];
+    echo $id." ".$first." ".$last." ".$email;
+    //$row = mysqli_fetch_assoc($ans);
+    //echo $row['Account_ID'];
     mysqli_close($conn);
 }
 
