81b9f2ec28c20d1d1ea4d90ba4067d22cc1ee79f
IvanTheHorrible <38411353+IvanTheHorrible@users.noreply.github.com>
2018-04-29 23:09:22 -0400

    Extra Element phone number and address POST added

--- Pingendo/php/infoAdded.php
+++ Pingendo/php/infoAdded.php
@@ -10,16 +10,16 @@ if(isset ($_POST("first_name")) // If the user came to this page by using edit a
       $lName = $_POST["last_name"];
 	  $gender = $_POST["gender"];
 	  $email = $_POST["email"];
-	  
-	  echo "The following has been updated:" . $fName . " " . $lName . " " .$gender . " " .$email;
+	  $number = $_POST("number");
+	  echo "The following has been updated:" . $fName . " " . $lName . " " .$gender . " " .$email." ". $number;
 }   
 else // User came to this page by using edit address info
 {
-	  $street = $_POST["first_name"];
-      $city = $_POST["last_name"];
-	  $city = $_POST["gender"];
-	  $state = $_POST["email"];
+	  $street = $_POST["street"];
+      $city = $_POST["city"];
+	  $state = $_POST["state"];
 	  $zip = $_POST("zip");
+	  echo "The following has been updated:" . $street . " " . $city . " " .$state . " " .$zip;
 }
 // leave for now require_once('../mysqli_connect.php');
 //$query = "UPDATE account_Info SET first_name = $f_name, last_name = $l_name, gender = $gender, email = $email";
