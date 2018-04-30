09d43dfc2dc83a57efcecc3a346c3fc348e1a336
IvanTheHorrible <38411353+IvanTheHorrible@users.noreply.github.com>
2018-04-30 01:50:10 -0400

    semi colon?

--- Pingendo/php/infoAdded.php
+++ Pingendo/php/infoAdded.php
@@ -5,22 +5,25 @@
 
 <?php
 if(isset ($_POST("first_name"))) // If the user came to this page by using edit account info
+{
       $fName = $_POST["first_name"];
       $lName = $_POST["last_name"];
 	  $gender = $_POST["gender"];
 	  $email = $_POST["email"];
 	  echo "The following has been updated:" . $fName . " " . $lName . " " .$gender . " " .$email;
 	  mysqli_query($dbc, "UPDATE INTO account_info( First_Name, Last_Name, Email, Gender)
-						  VALUES(" . $fName . ", " . $lName . ", " . $email . ", " . $gender . ",)")
+						  VALUES(" . $fName . ", " . $lName . ", " . $email . ", " . $gender . ",)");
+}
 else // User came to this page by using Create Group
+{
 	  $street = $_POST["street"];
       $city = $_POST["city"];
 	  $state = $_POST["state"];
 	  $zip = $_POST("zip");
 	  echo "The following has been updated:" . $street . " " . $city . " " .$state . " " .$zip;
 	 mysqli_query($dbc, "INSERT INTO family_group(Group_ID,Street_Address, City_Address, State_Address, Zip_Address)
-              VALUES(DEFAULT, " . $street .", ". $city . ", " . $state . ", " . $zip . ")")
-
+              VALUES(DEFAULT, " . $street .", ". $city . ", " . $state . ", " . $zip . ")");
+}
 
 ?>
 </body>
