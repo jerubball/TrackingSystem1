a7df59c551778e8a4f8563299ab2ff495698c423
Hasol Im <jerubball@gmail.com>
2018-04-30 01:15:49 -0400

    New Account

--- Pingendo/js/login-script.js
+++ Pingendo/js/login-script.js
@@ -29,7 +29,7 @@ function updateProfile (profile) {
     var xmlhttp = new XMLHttpRequest ();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            alert (this.responseText);
+            //alert (this.responseText);
         }
     };
     xmlhttp.open ("GET", "/php/login-profile.php?id=" + profile.getId() + 
