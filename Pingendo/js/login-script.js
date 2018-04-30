079d0d1c350c21e143da2608833ef86a32d6071d
Hasol Im <jerubball@gmail.com>
2018-04-30 01:08:19 -0400

    another update

--- Pingendo/js/login-script.js
+++ Pingendo/js/login-script.js
@@ -29,10 +29,10 @@ function updateProfile (profile) {
     var xmlhttp = new XMLHttpRequest ();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            //alert (this.responseText);
+            alert (this.responseText);
         }
     };
-    xmlhttp.open ("GET", "/php/login-cache.php?id=" + profile.getId() + 
+    xmlhttp.open ("GET", "/php/login-profile.php?id=" + profile.getId() + 
         "&first=" + profile.getGivenName() + "&last=" + profile.getFamilyName() + "&email=profile.getEmail()", true);
     xmlhttp.send ();
 }
