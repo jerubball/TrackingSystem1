8296842c446ddea34c425fd5e42a3abe8d8057c9
Hasol Im <jerubball@gmail.com>
2018-04-30 21:53:05 -0400

    test

--- Pingendo/js/profile-script.js
+++ Pingendo/js/profile-script.js
@@ -6,7 +6,7 @@ function getProfile () {
     var xmlhttp = new XMLHttpRequest ();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            alert (this.responseText);
+            //alert (this.responseText);
             var args = this.responseText.split (";");
             document.getElementById('accountFirst').value = args[0];
             document.getElementById('accountLast').value = args[1];
@@ -18,5 +18,17 @@ function getProfile () {
     xmlhttp.send ();
 }
 
-function updateProfile(id) {
+function updateProfile() {
+    var fname = document.getElementById('accountFirst').value;
+    var lname = document.getElementById('accountLast').value;
+    var email = document.getElementById('accountEmail').value;
+    var gender = document.getElementById('accountGender').value;
+    var xmlhttp = new XMLHttpRequest ();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //alert (this.responseText);
+        }
+    };
+    xmlhttp.open ("GET", "/php/update-profile.php?fname=" + fname + "&lname=" + lname + "&email=" + email + "&gender=" + gender, true);
+    xmlhttp.send ();
 }
---
+++ Pingendo/php/update-profile.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start ();
+
+$db_server = "localhost:3306";
+$db_user = "admin";
+$db_pass = "13579";
+
+if (isset($_SESSION['id'])) {
+    $id = $_SESSION['id'];
+    
+    $first = $_GET['fname'];
+    $last = $_GET['lname'];
+    $email = $_GET['email'];
+    $gender = $_GET['gender'];
+    
+    $conn = new mysqli ($db_server, $db_user, $db_pass);
+    
+    if ($conn -> connect_error) {
+      die ("Connection failed: " . $conn -> connect_error);
+    }
+    
+    $sql = "UPDATE Child_Tracker.account_info SET First_Name='$first', Last_Name='$last', Email='$email', Gender='$gender' WHERE Google_UID = ".$id;
+    $ans = $conn -> query($sql);
+    echo "Done";
+    $conn -> close();
+}
+
+?>
