cdd39ea73a677f282d2fef8baa50266cb687d716
Hasol Im <jerubball@gmail.com>
2018-05-01 21:05:06 -0400

    test

--- Pingendo/js/profile-script.js
+++ Pingendo/js/profile-script.js
@@ -6,14 +6,16 @@ function getProfile () {
     var xmlhttp = new XMLHttpRequest ();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            alert (this.responseText);
+            //alert (this.responseText);
             var args = this.responseText.split (";");
             document.getElementById('accountFirst').value = args[0];
             document.getElementById('accountLast').value = args[1];
             document.getElementById('accountEmail').value = args[2];
             document.getElementById('accountGender').value = args[3];
             var group = args[4];
-            
+            if (group == "") {
+                alert ("NULL");
+            }
         }
     };
     xmlhttp.open ("GET", "/php/get-profile.php", true);
