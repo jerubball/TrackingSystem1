eb77c17646363eed3dec48f5c82f095883eb6879
Hasol Im <jerubball@gmail.com>
2018-05-02 01:16:22 -0400

    update.

--- Pingendo/php/group-add.php
+++ Pingendo/php/group-add.php
@@ -23,8 +23,23 @@ if (isset($_SESSION['id'])) {
         $row = $ans->fetch_assoc();
         $group = $row['Group_ID'];
         
-        $sql = "UPDATE Child_Tracker.account_info SET Group_ID = '$group', Account_Type = 'Child' WHERE Email = '$email'";
+        $sql = "SELECT * FROM Child_Tracker.account_info WHERE Email = '$email'";
         $ans = $conn -> query($sql);
+        if ($ans->num_rows > 0) {
+            $row = $ans->fetch_assoc();
+            $target = $row['Group_ID'];
+            
+            if ($target == "") {
+                $sql = "UPDATE Child_Tracker.account_info SET Group_ID = '$group', Account_Type = 'Child' WHERE Email = '$email'";
+                $ans = $conn -> query($sql);
+            }
+            else {
+                echo "Account already belongs to group.";
+            }
+        }
+        else {
+            echo "No such account.";
+        }
         
         echo "Child Added.";
     }
