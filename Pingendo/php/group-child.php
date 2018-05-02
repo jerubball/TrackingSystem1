09ac876d9050430e6b755972b758b9674bc150b6
IvanTheHorrible <38411353+IvanTheHorrible@users.noreply.github.com>
2018-05-02 01:34:06 -0400

    group child

---
+++ Pingendo/php/group-child.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start ();
+
+$db_server = "localhost:3306";
+$db_user = "admin";
+$db_pass = "13579";
+
+if (isset($_SESSION['id'])) {
+    $id = $_SESSION['id'];
+$conn = new mysqli ($db_server, $db_user, $db_pass);
+   
+if ($conn -> connect_error) {
+      die ("Connection failed: " . $conn -> connect_error);
+    }
+    
+$sql = "SELECT Group_ID FROM Child_Tracker.account_info WHERE Google_UID = " . $id;
+$ans = $conn -> query($sql);
+if ($ans->num_rows > 0) {
+        $row = $ans->fetch_assoc();
+        $group = $row['Group_ID'];
+$sql = "SELECT First_Name FROM account_info WHERE Group_ID = "$group" AND Account_Type = Children"
+$ans = $conn -> query($sql);
+
+echo "Here Are the List:" . $ans;
