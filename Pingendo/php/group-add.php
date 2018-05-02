d678f8f777a404a27adf2a9e3a5861bcda7e277c
IvanTheHorrible <38411353+IvanTheHorrible@users.noreply.github.com>
2018-05-02 01:12:24 -0400

    remove

--- Pingendo/php/group-add.php
+++ Pingendo/php/group-add.php
@@ -6,13 +6,13 @@ if (isset($_SESSION['id'])) {
 $id = $_SESSION['id'];
 }
 
-$servername = "localhost:3306";
-$username = "admin";
-$password = "13579";
-$dbname = "Child_Tracker";
 
-$email = $_POST("email");
-$conn = new mysqli($servername, $username, $password, $dbname);
-$groupID = mysqli_query($conn, "SELECT Group_ID FROM account_info WHERE Google_UID = '" . $id . "'" );
+$db_server = "localhost:3306";
+$db_user = "admin";
+$db_pass = "13579";
 
- mysqli_query($conn, "UPDATE account_info SET Group_ID = '" . $groupID . "', Account_Type = Child WHERE Email = '". $email ."'); 
+$email = $_GET['email'];
+$conn = new mysqli ($db_server, $db_user, $db_pass);
+$groupID = mysqli_query($conn, "SELECT Group_ID FROM account_info WHERE Google_UID = " . $id" ");
+
+ mysqli_query($conn, "UPDATE account_info SET Group_ID = " . $groupID", Account_Type = Child WHERE Email = ". $email "); 
