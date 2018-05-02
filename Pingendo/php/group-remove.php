ae2052e186e01b143357bcc2f9125822a7f4e736
Hasol Im <jerubball@gmail.com>
2018-05-02 01:22:02 -0400

    update.

--- Pingendo/php/group-remove.php
+++ Pingendo/php/group-remove.php
@@ -1,10 +1,37 @@
-<?php 
+<?php
+
+session_start ();
+
+$db_server = "localhost:3306";
+$db_user = "admin";
+$db_pass = "13579";
+
 if (isset($_SESSION['id'])) {
-$id = $_SESSION['id'];
+    $id = $_SESSION['id'];
+    
+    $name = $_GET['name'];
+    
+    $conn = new mysqli ($db_server, $db_user, $db_pass);
+    
+    if ($conn -> connect_error) {
+      die ("Connection failed: " . $conn -> connect_error);
+    }
+    
+    $sql = "SELECT Group_ID FROM Child_Tracker.account_info WHERE Google_UID = " . $id;
+    $ans = $conn -> query($sql);
+    if ($ans->num_rows > 0) {
+        $row = $ans->fetch_assoc();
+        $group = $row['Group_ID'];
+        
+        $sql = "UPDATE Child_Tracker.account_info SET Group_ID = NULL, Account_Type = NULL WHERE Group_ID = '$group' AND First_Name = '$name' AND Account_Type = 'Child'";
+        $ans = $conn -> query($sql);
+        
+        echo "Child Removed.";
+    }
+    else {
+        echo "No result for account.";
+    }
+    $conn -> close();
 }
 
-$name = $_GET['name'];
-$conn = new mysqli ($db_server, $db_user, $db_pass);
-$groupID = mysqli_query($conn, "SELECT Group_ID FROM account_info WHERE Google_UID = " . $id "" );
-
- mysqli_query($conn, "UPDATE account_info SET Group_ID = NULL, Account_Type = NULL WHERE Group_ID = "$groupID" AND First_Name = "$name" AND Account_Type = Child");
+?>
