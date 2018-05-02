acbd5725761aae760ba8e640af00cbbf787ebc9c
Hasol Im <jerubball@gmail.com>
2018-05-01 23:46:27 -0400

    update

--- Pingendo/php/group-update.php
+++ Pingendo/php/group-update.php
@@ -27,13 +27,23 @@ if (isset($_SESSION['id'])) {
         $group = $row['Group_ID'];
         
         if ($group == "") {
-            $sql = "INSERT INTO Child_Tracker.family_group VALUES (DEFAULT, '$street', '$city', '$state', '$zip')";
+            $sql = "SHOW TABLE STATUS FROM `Child_Tracker` WHERE `Name` = 'family_group'";
             $ans = $conn -> query($sql);
-            
-            $sql = "UPDATE Child_Tracker.account_info SET Group_ID=LAST_INSERT_ID() WHERE Google_UID = ".$id;
-            $ans = $conn -> query($sql);
-            
-            echo "Created Group.";
+            if ($ans->num_rows > 0) {
+                $row = $ans->fetch_assoc();
+                $group = $row['Auto_Increment'];
+                
+                $sql = "INSERT INTO Child_Tracker.family_group VALUES ($group, '$street', '$city', '$state', '$zip')";
+                $ans = $conn -> query($sql);
+                
+                $sql = "UPDATE Child_Tracker.account_info SET Group_ID=$group WHERE Google_UID = ".$id;
+                $ans = $conn -> query($sql);
+                
+                echo "Created Group.";
+            }
+            else {
+                echo "Error."
+            }
         }
         else {
             $sql = "UPDATE Child_Tracker.family_group SET Street_Address='$street', City_Address='$city', State_Address='$state', Zip_Address=$zip WHERE Group_ID = ".$group;
