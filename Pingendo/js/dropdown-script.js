22ddb6e6c5d1d34397768fc779219ca1093f84cd
Hasol Im <jerubball@gmail.com>
2018-05-02 01:57:57 -0400

    test

--- Pingendo/js/dropdown-script.js
+++ Pingendo/js/dropdown-script.js
@@ -17,16 +17,19 @@ $(".dropdown-menu button").click(function(){
   $(this).parents(".btn-group").find('.btn').val($(this).data('value'));
 });
 
+function selectDropdown (id, txt) {
+    document.getElementById(id).innerHTML = txt;
+}
+
 function updateDropdown (id) {
     var xmlhttp = new XMLHttpRequest ();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             //alert (this.responseText);
-            alert (document.getElementById(id).innerHTML);
             var names = this.responseText.split (";");
             var htmltext = "";
             for (var i = 0; i < names.length - 1; i++) {
-                htmltext += "<button class=\"dropdown-item\" type=\"button\">" + names[i] + "</button>\n";
+                htmltext += "<button class=\"dropdown-item\" type=\"button\" onclick=\"selectDropdown('selectChildBtn', " + names[i] + ")\">" + names[i] + "</button>\n";
             }
             
             document.getElementById(id).innerHTML = htmltext;
