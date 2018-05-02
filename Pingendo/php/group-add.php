1a0e2e39183fd49638363a23aae53e73baab3d1f
Hasol Im <jerubball@gmail.com>
2018-05-02 01:03:42 -0400

    update.

--- Pingendo/php/group-add.php
+++ Pingendo/php/group-add.php
@@ -1,18 +1,37 @@
-<html> 
-<body>
+<?php
 
-<?php 
+session_start ();
+
+$db_server = "localhost:3306";
+$db_user = "admin";
+$db_pass = "13579";
+
 if (isset($_SESSION['id'])) {
-$id = $_SESSION['id'];
+    $id = $_SESSION['id'];
+    
+    $email = $_GET("email");
+    
+    $conn = new mysqli ($db_server, $db_user, $db_pass);
+    
+    if ($conn -> connect_error) {
+      die ("Connection failed: " . $conn -> connect_error);
+    }
+    
+    $sql = "SELECT Group_ID FROM Child_Tracker.account_info WHERE Google_UID = " . $id;
+    $ans = $conn -> query($sql);
+    if ($ans->num_rows > 0) {
+        $row = $ans->fetch_assoc();
+        $group = $row['Group_ID'];
+        
+        $sql = "UPDATE Child_Tracker.account_info SET Group_ID = '$groupID', Account_Type = 'Child' WHERE Email = '$email");
+        $ans = $conn -> query($sql);
+        
+        echo "Child Added."
+    }
+    else {
+        echo "No result for account.";
+    }
+    $conn -> close();
 }
 
-$servername = "localhost:3306";
-$username = "admin";
-$password = "13579";
-$dbname = "Child_Tracker";
-
-$email = $_POST("email");
-$conn = new mysqli($servername, $username, $password, $dbname);
-$groupID = mysqli_query($conn, "SELECT Group_ID FROM account_info WHERE Google_UID = '" . $id . "'" );
-
- mysqli_query($conn, "UPDATE account_info SET Group_ID = '" . $groupID . "', Account_Type = Child WHERE Email = '". $email ."'); 
+?>
