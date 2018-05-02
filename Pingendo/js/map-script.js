a2b0a887cf1a676bf3496fd6cb319a545151bf63
Hasol Im <jerubball@gmail.com>
2018-05-02 02:40:13 -0400

    test

--- Pingendo/js/map-script.js
+++ Pingendo/js/map-script.js
@@ -1,18 +1,48 @@
 
 function map1() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showMap1);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showMap1);
+    }
+    else {
+        alert("Geolocation is not supported by this browser.");
+    }
 }
 
-function map2 () {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showMap1);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
+function updateMap1 () {
+    var name = document.getElementById('selectChildBtn').innerHTML;
+    var xmlhttp = new XMLHttpRequest ();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          //alert (this.responseText);
+          var coords = this.responseText.split(";");
+          var centers = [];
+          for (var i = 0; i < coords.length - 1; i++) {
+              var coord = coords[i].split(",");
+              //alert (parseFloat(coord[0]) + ", " + parseFloat(coord[1]));
+              centers.push (new google.maps.LatLng(parseFloat(coord[0]), parseFloat(coord[1])));
+          }
+          var myCenter = centers[centers.length - 1];
+          var mapCanvas = document.getElementById("map1");
+          var mapOptions = {center: myCenter, zoom: 12};
+          var map = new google.maps.Map(mapCanvas, mapOptions);
+          var flightPath = new google.maps.Polyline({
+            path: centers,
+            strokeColor: "#0000FF",
+            strokeOpacity: 0.8,
+            strokeWeight: 2
+          });
+          flightPath.setMap(map);
+          var marker = new google.maps.Marker({position:myCenter});
+          marker.setMap(map);
+          var cord = coords[coords.length - 2].split(",");
+          var infowindow = new google.maps.InfoWindow({
+            content: name + "'s Location.<br>Latitude: " + cord[0] + "<br>Longitude: " + cord[1]
+          });
+          infowindow.open(map,marker);
+        }
+      };
+      xmlhttp.open ("GET", "/php/map-retrieve.php?name=" + name, true);
+      xmlhttp.send ();
 }
 
 function showMap1 (position) {
@@ -47,7 +77,7 @@ function showMap1 (position) {
       infowindow.open(map,marker);
     }
   };
-  xmlhttp.open ("GET", "/php/map-retrieve.php", true);
+  xmlhttp.open ("GET", "/php/map-retrieve.php?name=", true);
   xmlhttp.send ();
   
 }
--- Pingendo/php/map-retrieve.php
+++ Pingendo/php/map-retrieve.php
@@ -9,31 +9,68 @@ $db_pass = "13579";
 if (isset($_SESSION['id'])) {
     $id = $_SESSION['id'];
     
+    $name = $_GET['name'];
+    
     $conn = new mysqli ($db_server, $db_user, $db_pass);
     
     if ($conn -> connect_error) {
       die ("Connection failed: " . $conn -> connect_error);
     }
     
-    $sql = "SELECT Account_ID FROM Child_Tracker.account_info WHERE Google_UID = " . $id;
-    $ans = $conn -> query($sql);
-    $row = mysqli_fetch_assoc($ans);
-    $usr = $row['Account_ID'];
-    
-    //$sql = "SELECT COUNT(*) AS num FROM Child_Tracker.location WHERE Account_ID = " . $usr;
-    //$ans = $conn -> query($sql);
-    //$row = mysqli_fetch_assoc($ans);
-    //$num = intval($row['num']);
-    
-    $sql = "SELECT * FROM Child_Tracker.location WHERE Account_ID = " . $usr;
-    $ans = $conn -> query($sql);
-    if ($ans->num_rows > 0) {
-        while($row = $ans->fetch_assoc()) {
-            echo $row['Latitude'] . "," . $row['Longitude']. ";";
+    if ($name == "") {
+        $sql = "SELECT * FROM Child_Tracker.account_info WHERE Google_UID = " . $id;
+        $ans = $conn -> query($sql);
+        if ($ans->num_rows > 0) {
+            $row = $ans->fetch_assoc();
+            $usr = $row['Account_ID'];
+            
+            //$sql = "SELECT COUNT(*) AS num FROM Child_Tracker.location WHERE Account_ID = " . $usr;
+            //$ans = $conn -> query($sql);
+            //$row = mysqli_fetch_assoc($ans);
+            //$num = intval($row['num']);
+            
+            $sql = "SELECT * FROM Child_Tracker.location WHERE Account_ID = " . $usr;
+            $ans = $conn -> query($sql);
+            if ($ans->num_rows > 0) {
+                while($row = $ans->fetch_assoc()) {
+                    echo $row['Latitude'] . "," . $row['Longitude']. ";";
+                }
+            }
+            else {
+                echo "No result for Location.";
+            }
         }
     }
     else {
-        echo "No result for Location.";
+        $sql = "SELECT * FROM Child_Tracker.account_info WHERE Google_UID = " . $id;
+        $ans = $conn -> query($sql);
+        if ($ans->num_rows > 0) {
+            $row = $ans->fetch_assoc();
+            $group = $row['Group_ID'];
+            
+            $sql = "SELECT * FROM Child_Tracker.account_info WHERE Group_ID = '$group' AND First_Name = '$name' AND Account_Type = 'Child'";
+            $ans = $conn -> query($sql);
+            if ($ans->num_rows > 0) {
+                $row = $ans->fetch_assoc();
+                $usr = $row['Account_ID'];
+                
+                //$sql = "SELECT COUNT(*) AS num FROM Child_Tracker.location WHERE Account_ID = " . $usr;
+                //$ans = $conn -> query($sql);
+                //$row = mysqli_fetch_assoc($ans);
+                //$num = intval($row['num']);
+                
+                $sql = "SELECT * FROM Child_Tracker.location WHERE Account_ID = " . $usr;
+                $ans = $conn -> query($sql);
+                if ($ans->num_rows > 0) {
+                    while($row = $ans->fetch_assoc()) {
+                        echo $row['Latitude'] . "," . $row['Longitude']. ";";
+                    }
+                }
+                else {
+                    echo "No result for Location.";
+                }
+            }
+        }
     }
     $conn -> close();
 }
