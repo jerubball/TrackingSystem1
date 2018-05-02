230ec5487742e30d9c594951fd8db3c5f6e70f2e
Hasol Im <jerubball@gmail.com>
2018-05-01 23:07:35 -0400

    update 3

--- Pingendo/php/group-update.php
+++ Pingendo/php/group-update.php
@@ -30,18 +30,8 @@ if (isset($_SESSION['id'])) {
             $sql = "INSERT INTO Child_Tracker.family_group VALUES (DEFAULT, '$street', '$city', '$state', '$zip')";
             $ans = $conn -> query($sql);
             
-            $sql = "SELECT * FROM Child_Tracker.account_info WHERE Google_UID = " . $id;
+            $sql = "UPDATE Child_Tracker.account_info SET Group_ID=LAST_INSERT_ID() WHERE Google_UID = ".$id;
             $ans = $conn -> query($sql);
-            if ($ans->num_rows > 0) {
-                $row = $ans->fetch_assoc();
-                $group = $row['Group_ID'];
-                echo "$group";
-                $sql = "UPDATE Child_Tracker.account_info SET Group_ID=$group WHERE Google_UID = ".$id;
-                $ans = $conn -> query($sql);
-            }
-            else {
-                echo "Error adding account.";
-            }
         }
         else {
             $sql = "UPDATE Child_Tracker.family_group SET Street_Address='$street', City_Address='$city', State_Address='$state', Zip_Address=$zip WHERE Group_ID = ".$group;
