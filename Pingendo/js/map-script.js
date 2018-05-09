8fba2d1690ce01ff2fcaca8e8ea0b310a3d9c3c6
Hasol Im <jerubball@gmail.com>
2018-05-09 01:08:26 -0400

    Last commit before presentation.

--- Pingendo/js/map-script.js
+++ Pingendo/js/map-script.js
@@ -82,7 +82,7 @@ function showMap1 (position) {
   var xmlhttp = new XMLHttpRequest ();
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      alert (this.responseText);
+      //alert (this.responseText);
       var coords = this.responseText.split(";");
       var centers = [];
       for (var i = 0; i < coords.length - 1; i++) {
