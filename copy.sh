ba58e61566b31c3394ac95dba2df207a1c0d74c3
Hasol Im <jerubball@gmail.com>
2019-06-19 22:09:59 -0400

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cp -rf ./Application/* /usr/local/apache2/htdocs/TrackingSystem/
+#cp -rf ./Application/* /usr/local/apache2/htdocs/TrackingSystem/
+cp -rf ./Application/* /var/www/html/TrackingSystem/
